300f2c1b78be3cad458484e71c3cbcc36795d2dc
anh vo <anhvth.226@gmail.com>
2023-12-18 16:24:15 +0000

    first working qwen

--- guidance/models/transformers/_qwen.py
+++ guidance/models/transformers/_qwen.py
@@ -1,9 +1,12 @@
 import torch
+from tqdm import tqdm
 from ._transformers import Transformers
 from ._transformers import TransformersChat
 from loguru import logger
 
 import time
+
+
 class Qwen(Transformers):
     def __init__(
         self,
@@ -32,14 +35,24 @@ class Qwen(Transformers):
         )
         self.time_table = {}
 
+    def decode(self, ids):
+        return self._orig_tokenizer.decode(ids)
+
+    def encode(self, text):
+        return self._orig_tokenizer.encode(text)
+
     def _get_logits(self, token_ids, forced_bytes, current_temp, **kwargs):
         """Computes the logits for the given token state.
 
         This overrides a method from the LocalEngine class that is used to get
         inference results from the model.
         """
-        start = time.time()
         # make sure we don't run off the end of the model
+
+        _text = self.decode(token_ids)
+        # new_token_ids = self.encode(_text)
+        # assert len(new_token_ids) == len(token_ids)
+
         if len(token_ids) >= getattr(
             self.model_obj.config, "max_position_embeddings", 1e10
         ):
@@ -48,6 +61,7 @@ class Qwen(Transformers):
             )
 
         # get the number of cache positions we are using
+        # import ipdb; ipdb.set_trace()
         cache_token_ids = self._cache_state["cache_token_ids"]
         num_cached = 0
         for id in cache_token_ids:
@@ -75,12 +89,15 @@ class Qwen(Transformers):
 
         # call the model
         new_token_ids = token_ids[past_length:]
+        # print('Past length', past_length, 'new length', len(new_token_ids))
+        if len(new_token_ids)==0:
+            import ipdb; ipdb.set_trace()
         if len(new_token_ids) > 0:
             with torch.no_grad():
                 model_out = self.model_obj(
                     input_ids=torch.tensor(new_token_ids).unsqueeze(0).to(self.device),
                     past_key_values=self._cache_state["past_key_values"],
-                    use_cache=True,
+                    use_cache=False,
                     position_ids=torch.arange(
                         past_length, past_length + len(new_token_ids)
                     )
@@ -97,21 +114,47 @@ class Qwen(Transformers):
             # save the results
             self._cache_state["past_key_values"] = model_out.past_key_values
             cache_token_ids.extend(new_token_ids)
-            self._cache_state["logits"] = (
-                model_out.logits[0, -1, :].float().cpu().numpy()
-            )
-            
-            max_token = self._cache_state["logits"].argmax()
-            token = self.tokens[max_token]
-            self.time_table[token] = time.time() - start
+            logits = model_out.logits[0, -1, :].float()
+            # if self.compute_log_probs:
+                # logits = logits.softmax(-1)
+            self._cache_state["logits"] = logits.cpu().numpy()
+            scores, ids = model_out.logits[0, -1, :].softmax(-1).topk(2)
+            topk = {}
+            for i in range(len(ids)):
+                topk[self.decode([ids[i].item()])] = scores[i].item()
+            # print("topk", topk)
+            # import ipdb;ipdb.set_trace()  # pred_token = self.decode(x[1])
 
         return self._cache_state["logits"]
 
-    # def _tokenize_prefix(self, prompt):
-    #     if isinstance(prompt, bytes):
-    #         prompt = prompt.decode("utf-8")
-    #     return self._orig_tokenizer(prompt).input_ids, []
+    def _cleanup_tokens(self, token_ids, token_byte_positions):
+        return token_ids, token_byte_positions
 
+    def _tokenize_prefix(self, byte_string):
+        string = str(byte_string, encoding="utf8")
+        # if '>assistant' in string:
+        #     last_idx_of_assistant = string.rfind('>assistant')
+        #     prefix = string[:last_idx_of_assistant]
+            # prefix, assistant_prefix = string.split('>assistant')
+            
+        token_ids = self.encode(string)
+        bytes_position = []
 
-class QwenChat(Qwen, TransformersChat):
+        for i in range(len(token_ids)):
+            _ids = token_ids[: i + 1]
+            _s = self.decode(_ids)
+            _bytes = bytes(_s, encoding="utf8")
+            bytes_position.append(len(_bytes))
+            
+        posible_end_tokens = []
+        if len(bytes_position):
+            last_byte = _bytes[bytes_position[-2]:]
+            for token in self.tokens:
+                if token.startswith(last_byte):
+                    posible_end_tokens.append(token)
+            if len(posible_end_tokens):
+                return token_ids[: -1], bytes_position[: -1]
+        return token_ids, bytes_position
+    
+class QwenChat(Qwen):
     pass
